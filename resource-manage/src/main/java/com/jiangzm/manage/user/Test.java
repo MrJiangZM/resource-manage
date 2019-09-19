ef15c1159e7963aa2b7e6a5ea1714dee76d95e92
0002693 <jiangzaiming@mininglamp.com>
2019-09-19 20:44:40 +0800

    添加设置.gitignore

---
+++ resource-manage/src/main/java/com/jiangzm/manage/user/Test.java
@@ -0,0 +1,14 @@
+package com.jiangzm.manage.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+
+    @GetMapping("/test")
+    public void test() {
+        System.out.println("=================");
+    }
+
+}
